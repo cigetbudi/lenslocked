50044c23116fdd2721b09c7a640cc377d74a8020
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-07-10 20:39:51 +0700

    Add Contact Page

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html, charset=utf-8") //DEFAULT
 	fmt.Fprint(w, "<h1>SELAMAT DATANG</h1>")
 }
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html, charset=utf-8") //DEFAULT
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>Untuk lebih lanjut, hubungi saya ke <a href=\"mailto:cigetbudi@gmail.com\">cigetbudi@gmail.com</a>.")
+}
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Mengaktifkan server di :3000...")
 	http.ListenAndServe(":3000", nil)
 }
